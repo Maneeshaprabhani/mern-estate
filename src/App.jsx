14a7dd01fa1fe49173377a431bc9cc35acd14a6f
Maneeshaprabhani <137678004+Maneeshaprabhani@users.noreply.github.com>
2024-04-07 16:49:46 +0530

    add create listing api route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Signin from './pages/Signin';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
-
-
-
-
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './pages/Home'
+import Sigin from "./pages/Sigin"
+import SignUp from './pages/SignUp'
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/Header"
+import PrivateRoute from "./components/PrivateRoute"
 
 
 
 export default function App() {
   return (
+
     <BrowserRouter>
+
     <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route element={<PrivateRoute/>} >
-        <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Sigin />} />
+      <Route path="/sign-up" element={<SignUp />} /> 
+      <Route path="/about" element={<About />} />
+      <Route element={<PrivateRoute />}>
+      <Route path="/profile" element={<Profile/>} />
+      </Route>
+    </Routes>
     </BrowserRouter>
-  );
+  )
 }
--- src/pages/Sigin.jsx
+++ src/pages/Sigin.jsx
@@ -8,19 +8,17 @@ import {
 } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
-export default function SignIn() {
+export default function Signin() {
   const [formData, setFormData] = useState({});
-  const {loading,error} = useSelector((state) => state.user);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
   const handleChange = (e) => {
-    setFormData  ({
+    setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -44,27 +42,25 @@ export default function SignIn() {
       dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='email'
-          placeholder='Email'
+          placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-          value={formData.email}
           onChange={handleChange}
         />
         <input
           type='password'
-          placeholder='Password'
+          placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-          value={formData.password}
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
@@ -74,8 +70,10 @@ export default function SignIn() {
         <OAuth/>
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Don't have an account?</p>
-        <Link to='/sign-up' className='text-blue-700'>Sign up</Link>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
